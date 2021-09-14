6d4f4e198895e4d277945efb895cd6250bf72cbb
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-09-13 22:10:46 -0400

    first commit

---
+++ app/src/main/java/com/example/nybooks/ui/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nybooks.ui.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
