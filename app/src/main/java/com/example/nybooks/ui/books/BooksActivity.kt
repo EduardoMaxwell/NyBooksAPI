490d17708cbb6fdfe657f9d8c9164e4c17f8316e
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-09-14 20:27:13 -0400

    Changed application theme. Added support to binding. Add Recyler and Toolbar

--- app/src/main/java/com/example/nybooks/ui/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/ui/books/BooksActivity.kt
@@ -3,10 +3,18 @@ package com.example.nybooks.ui.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nybooks.R
+import com.example.nybooks.databinding.ActivityBooksBinding
 
 class BooksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBooksBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_books)
+        binding = ActivityBooksBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.toolbarMain.title = getString(R.string.books_title)
+        setSupportActionBar(binding.toolbarMain)
     }
 }
